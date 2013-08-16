e03b2fb5121ea3fe21b0152e0bb447db03d4df7f
Marc Pare <marc@smallredtile.com>
2013-08-15 22:31:15 -0700

    fix image url route

--- site/master.inc.php
+++ site/master.inc.php
@@ -8,7 +8,7 @@ $config = Config::getConfig();
 
 define('WEB_ROOT', $config->web_root);
 define('URL_ROOT', 'http://' . $_SERVER['HTTP_HOST'] . '/' . WEB_ROOT);
-define('URL_IMG_ROOT', 'http://localhost:8888/ricehuskcc/img/');
-define('URL_CSS_ROOT', 'http://localhost:8888/ricehuskcc/css/');
+define('URL_IMG_ROOT', URL_ROOT . 'img/');
+define('URL_CSS_ROOT', URL_ROOT . 'css/');
 
 ?>
